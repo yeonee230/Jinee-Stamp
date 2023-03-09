71f19610b67382b04d3ee6bd125de2709f10df2f
SUYEON KIM <yeonee230@gmail.com>
2023-03-09 23:38:00 +0900

    logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import "dotenv/config";
-
+import morgan from "morgan";
 import getNotionApi from './notion';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const logger = morgan("dev");
 
 app.use(express.static(__dirname + "/public"));
+app.use(logger);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,11 +25,13 @@ app.use((req, res, next) => {
 app.get("/", async (req, res) => {
     try {
         const notionData = await getNotionApi();
-        res.send(notionData);
+        console.log(notionData);
+        res.sendFile(__dirname + "/views/index.html",notionData);
       } catch (err) {
         console.error(err);
       }
-  //res.sendFile(__dirname + "/views/index.html");
+
+  
 });
 
 app.listen(PORT, () => console.log(`⭐️ Conneted server! PORT : ${PORT}`));
