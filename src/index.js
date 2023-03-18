2bcf5fd7a4c70f6f366f1e1dab8bce0d967cca8b
SUYEON KIM <yeonee230@gmail.com>
2023-03-18 19:36:18 +0900

    frontend setting

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views"); //현재작업폴더(cwd) = node.js를 실행하는 파일(package.js)이 있는 폴더임.
 
-//app.use(express.static(__dirname + "/public"));
 
 app.use(logger);
+app.use("/static",express.static("assets")); // /assets 아니어도 됨 url은 마음대로 설정 가능 
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -28,6 +28,8 @@ app.use((req, res, next) => {
   // res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
+
+// url 설정 --------------------------------------------------------------------------------//
 
 app.get("/", async (req, res) => {
   try {
